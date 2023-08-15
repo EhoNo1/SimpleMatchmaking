11a693617d040e67e273ac21c2e7ac684e6e9233
Evan Overly <Aetous@protonmail.com>
2023-08-14 19:08:17 -0500

    Began work on server form

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ serverBrowser.addServer("000.000.000.000","JohnDoe",25565,"1.0","ExampleGame",tr
 
 
 
-
 app.get('/', async function(req,res) {
     res.json({test:"woah"});
 });
@@ -18,14 +17,19 @@ app.get('/serverlist', async function(req,res) {
     res.json(serverBrowser.getServerList());
 })
 
+app.post('/registerserver', async function(req,res) {
+    //req.body
+    console.log(req.body);
+    res.json("no");
+});
 
 
+
+app.use(express.jsonencoded({ex}))
 app.use(function (req, res, next) {
     res.status(404);
     res.render("404");
 });
-
-
 
 
 //make app listenable
