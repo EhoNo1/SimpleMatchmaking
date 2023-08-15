a5830ee573775443e36e333d77e7f8895719ea5e
Evan Overly <EvanHOverly@gmail.com>
2023-08-14 22:21:21 -0500

    Successfully received a form request from a game

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SimpleMatchmaking
-A generic simple matchmaking server for your multiplayer needs
+A generic simple game-independent matchmaking server for your multiplayer needs
+
+# Installation
+**Warning:** This project is currently still in development and has no releases ready. It may not be ready for deployment and may contain dangerous security vulnerabilities. Use only after personal inspection at your own risk.
+
+1. Install NodeJS on your host device. https://nodejs.org/en
+2. Clone or download this repo into your host device.
+3. In a command terminal, run `npm install` in the project folder to download and update any needed dependencies.
+
+# Updating
+1. If cloned, run `git pull` in a command terminal to download updates. If downloaded directly, you'll need to redownload the repo again.
+2. In the project folder, run `npm install` to update any dependencies that may have changed between updates.
+
+# Running
+Use `npm index.js` in a command terminal to start the webservice.
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let config = require("./config.json")
-console.log(config)
-let serverBrowser = require("./lib/serverbrowser")
-const express = require("express")
+let config = require("./config.json");
+console.log(config);
+let serverBrowser = require("./lib/serverbrowser");
+const express = require("express");
 let app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
-serverBrowser.addServer("000.000.000.000","JohnDoe",25565,"1.0","ExampleGame",true,false)
+//serverBrowser.addServer("000.000.000.000","JohnDoe",25565,"1.0","ExampleGame",true,false)
+
 
 
 
@@ -20,12 +23,18 @@ app.get('/serverlist', async function(req,res) {
 app.post('/registerserver', async function(req,res) {
     //req.body
     console.log(req.body);
-    res.json("no");
+    if (req.body) {
+        serverBrowser.addServer(req.body.hostIP,"Filler",25565,"1.0","ExampleGame",true,false);
+        res.status(201);
+        res.send(201);
+    } else {
+        console.log("Empty request");
+        res.status(400); //Bad request
+        res.send(404);
+    }
 });
 
 
-
-app.use(express.jsonencoded({ex}))
 app.use(function (req, res, next) {
     res.status(404);
     res.render("404");
